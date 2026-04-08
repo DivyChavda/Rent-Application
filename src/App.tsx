7080ea111a4e7d4c7c94f4b57642f2cd6a6a7a8d
Divy Chavda <62927648+DivyChavda@users.noreply.github.com>
2026-04-08 15:15:37 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RentElectricityCalculator from './pages/rentElectricityCalculator';
+
+function App() {
+  return <RentElectricityCalculator />;
+}
+
+export default App;
